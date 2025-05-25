706eeed72bdb65e2f232132f1b376316c97a2fb4
nmusey <nmusey@gmail.com>
2025-05-25 11:46:20 -0700

    Fix TodoAdd command

--- lua/todo/commands.lua
+++ lua/todo/commands.lua
@@ -1,6 +1,15 @@
 local function register_commands()
     local todo = require('todo.todo')
-    vim.api.nvim_create_user_command('TodoAdd', todo.add_task, { desc = 'Add a new task' })
+
+    vim.api.nvim_create_user_command('TodoAdd', function(opts)
+        local args = vim.split(opts.args, " ", { plain = true })
+        if #args == 0 then
+            print("Usage: TodoAdd <task_text>")
+            return
+        end
+        M.add_task(table.concat(args, " "))
+    end, { nargs = "*", desc = 'Add a new todo task' })
+
     vim.api.nvim_create_user_command('TodoList', todo.view_todo_list, { desc = 'View the list of tasks' })
     vim.api.nvim_create_user_command('TodoFinish', todo.finish_task, { desc = 'Mark a task as finished' })
     vim.api.nvim_create_user_command('TodoDelete', todo.delete_task, { desc = 'Delete a task' })
