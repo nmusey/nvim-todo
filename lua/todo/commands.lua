af723c790cc6e264e152294659b7eea01fcb0c23
nmusey <nmusey@gmail.com>
2025-05-25 09:54:24 -0700

    Hopefully fix the plugin

--- lua/todo/commands.lua
+++ lua/todo/commands.lua
@@ -1,12 +1,12 @@
 local function register_commands()
     local todo = require('todo.todo')
-    vim.api.nvim_create_command('TodoAdd', todo.add_task, { desc = 'Add a new task' })
-    vim.api.nvim_create_command('TodoList', todo.view_todo_list, { desc = 'View the list of tasks' })
-    vim.api.nvim_create_command('TodoFinish', todo.finish_task, { desc = 'Mark a task as finished' })
-    vim.api.nvim_create_command('TodoDelete', todo.delete_task, { desc = 'Delete a task' })
-    vim.api.nvim_create_command('TodoEdit', todo.edit_task, { desc = 'Edit a task' })
-    vim.api.nvim_create_command('TodoPrioritize', todo.prioritize_task, { desc = 'Prioritize a task' })
-    vim.api.nvim_create_command('Todotodo.nu', M.todo_menu, { desc = 'Open the todo menu' })
+    vim.api.nvim_create_user_command('TodoAdd', todo.add_task, { desc = 'Add a new task' })
+    vim.api.nvim_create_user_command('TodoList', todo.view_todo_list, { desc = 'View the list of tasks' })
+    vim.api.nvim_create_user_command('TodoFinish', todo.finish_task, { desc = 'Mark a task as finished' })
+    vim.api.nvim_create_user_command('TodoDelete', todo.delete_task, { desc = 'Delete a task' })
+    vim.api.nvim_create_user_command('TodoEdit', todo.edit_task, { desc = 'Edit a task' })
+    vim.api.nvim_create_user_command('TodoPrioritize', todo.prioritize_task, { desc = 'Prioritize a task' })
+    vim.api.nvim_create_user_command('Todotodo.nu', M.todo_menu, { desc = 'Open the todo menu' })
 end
 
 return {
