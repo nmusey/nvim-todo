2e3986655011795f610e10951897d89964ff7dfb
Nick Musey <nmusey@gmail.com>
2025-05-26 22:44:24 -0700

    Refactor commands

--- lua/todo/commands.lua
+++ lua/todo/commands.lua
@@ -1,16 +1,59 @@
+local todo = require('todo.todo')
+
+local function add_task_prompt()
+    local task_text
+    vim.ui.input({ prompt = "Enter task text: " }, function(str) task_text = str end)
+    if task_text and #task_text > 0 then
+        todo.add_task(task_text)
+    else
+        print("Task text cannot be empty.")
+    end
+end
+
+local function finish_task_prompt()
+    local task_id
+    vim.ui.input({ prompt = "Enter task ID: " }, function (str) task_id = str end)
+    if task_id and #task_id > 0 then
+        todo.finish_task(task_id)
+    else
+        print("Task ID cannot be empty.")
+    end
+end
+
+local function delete_task_prompt()
+    local task_id = vim.ui.input({ prompt = "Enter task ID: " })
+    if task_id and #task_id > 0 then
+        todo.delete_task(task_id)
+    else
+        print("Task ID cannot be empty.")
+    end
+end
+
+local function edit_task_prompt()
+    local args = vim.split(opts.args, " ", { plain = true })
+    if #args ~= 2 then
+        print("Usage: TodoEdit <task_id> <new_text>")
+        return
+    end
+    todo.edit_task(args[1], args[2])
+end
+
+local function prioritize_task_prompt()
+    local args = vim.split(opts.args, " ", { plain = true })
+    if #args ~= 2 then
+        print("Usage: TodoPrioritize <task_id> <priority>")
+        return
+    end
+    todo.prioritize_task(args[1], args[2])
+end
+
 local function register_commands()
-    local todo = require('todo.todo')
 
     vim.api.nvim_create_user_command('TodoAdd', function(opts)
         if opts.args then
             todo.add_task(opts.args)
         else
-            local task_text = vim.ui.input({ prompt = "Enter task text: " })
-            if task_text and #task_text > 0 then
-                todo.add_task(task_text)
-            else
-                print("Task text cannot be empty.")
-            end
+            add_task_prompt()
         end
     end, { nargs = "?", desc = 'Add a new todo task' })
 
@@ -22,12 +65,7 @@ local function register_commands()
         if opts.args then
             todo.finish_task(opts.args)
         else
-            local task_id = vim.ui.input({ prompt = "Enter task ID: " })
-            if task_id and #task_id > 0 then
-                todo.finish_task(task_id)
-            else
-                print("Task ID cannot be empty.")
-            end
+            finish_task_prompt()
         end
     end, { nargs = "?", desc = 'Mark a task as finished' })
 
@@ -35,12 +73,7 @@ local function register_commands()
         if opts.args then
             todo.delete_task(opts.args)
         else
-            local task_id = vim.ui.input({ prompt = "Enter task ID: " })
-            if task_id and #task_id > 0 then
-                todo.delete_task(task_id)
-            else
-                print("Task ID cannot be empty.")
-            end
+            delete_task_prompt()
         end
     end, { nargs = "?", desc = 'Delete a task' })
 
@@ -48,12 +81,7 @@ local function register_commands()
         if opts.args and #opts.args == 2 then
             todo.edit_task(opts.args[1], opts.args[2])
         else
-            local args = vim.split(opts.args, " ", { plain = true })
-            if #args ~= 2 then
-                print("Usage: TodoEdit <task_id> <new_text>")
-                return
-            end
-            todo.edit_task(args[1], args[2])
+            edit_task_prompt()
         end
     end, { nargs = "?", desc = 'Edit a task' })
 
@@ -61,63 +89,23 @@ local function register_commands()
         if opts.args and #opts.args == 2 then
             todo.prioritize_task(opts.args[1], opts.args[2])
         else
-            local args = vim.split(opts.args, " ", { plain = true })
-            if #args ~= 2 then
-                print("Usage: TodoPrioritize <task_id> <priority>")
-                return
-            end
-            todo.prioritize_task(args[1], args[2])
+            prioritize_task_prompt()
         end
     end, { nargs = "?", desc = 'Set task priority (1-3)' })
 
     vim.api.nvim_create_user_command('TodoMenu', function()
         local choices = {
-            "Add Task",
-            "View List",
-            "Finish Task",
-            "Delete Task",
-            "Edit Task",
-            "Prioritize Task"
+            ["Add Task"] = add_task_prompt(),
+            ["View List"] = function() todo.view_todo_list() end,
+            ["Finish Task"] = function() finish_task_prompt() end,
+            ["Delete Task"] = function() delete_task_prompt() end,
+            ["Edit Task"] = function() edit_task_prompt() end,
+            ["Prioritize Task"] = function() prioritize_task_prompt() end
         }
 
         vim.ui.select(choices, { prompt = "Select an option: " }, function(choice)
-            if choice == "Add Task" then
-                local task_text = vim.ui.input({ prompt = "Enter task text: " })
-                if task_text and #task_text > 0 then
-                    todo.add_task(task_text)
-                else
-                    print("Task text cannot be empty.")
-                end
-            elseif choice == "View List" then
-                todo.view_todo_list()
-            elseif choice == "Finish Task" then
-                local task_id = vim.ui.input({ prompt = "Enter task ID: " })
-                if task_id and #task_id > 0 then
-                    todo.finish_task(task_id)
-                else
-                    print("Task ID cannot be empty.")
-                end
-            elseif choice == "Delete Task" then
-                local task_id = vim.ui.input({ prompt = "Enter task ID: " })
-                if task_id and #task_id > 0 then
-                    todo.delete_task(task_id)
-                else
-                    print("Task ID cannot be empty.")
-                end
-            elseif choice == "Edit Task" then
-                local args = vim.split(opts.args, " ", { plain = true })
-                if #args ~= 2 then
-                    print("Usage: TodoEdit <task_id> <new_text>")
-                    return
-                end
-                todo.edit_task(args[1], args[2])
-            elseif choice == "Prioritize Task" then
-                local args = vim.split(opts.args, " ", { plain = true })
-                if #args ~= 2 then
-                    print("Usage: TodoPriority <task_id> <priority>")
-                    return
-                end
-                todo.prioritize_task(args[1], args[2])
+            if choice then
+                tasks_dict[choice]()
             end
         end)
     end, { desc = 'Open the todo menu' })
